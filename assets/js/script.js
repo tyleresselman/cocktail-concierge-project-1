68d482a2176fcf1169833830eb28e18fc6da49b2
Tyler Esselman <tme91302@gmail.com>
2022-09-16 10:17:50 -0700

    worked on validation for ingredient search on our own and then with Poornima

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ var cocktailBtn = $("#cocktail-btn");
 var ingredientBtn = $("#ingredient-btn");
 var nameButtonDiv = $(".name-button-div");
 var ingButtonDiv = $(".ing-button-div");
+var ingListSelectEl = $("#ing-list");
 
 cocktailBtn.on("click", function(e) {
     e.preventDefault();
@@ -18,9 +19,34 @@ cocktailBtn.on("click", function(e) {
     }
 });
 
+function validatingIngInput() {
+    var ingListUrl = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+  
+  fetch(ingListUrl)
+    .then(function(response){
+      return response.json()
+      
+    }).then(function(data){
+      // console.log(data); 
+      // strIngredient1
+      for (let i = 0; i < data.drinks.length; i++) {
+        // console.log(data.drinks[i].strIngredient1);
+      var optionEl = $("<option>");
+      optionEl.text(data.drinks[i].strIngredient1);
+      optionEl.val(data.drinks[i].strIngredient1);
+      ingListSelectEl.append(optionEl)
+        
+      }
+  
+    })
+  }
+  validatingIngInput();
+
 ingredientBtn.on("click", function(e) {
     e.preventDefault();
     var ingredientVal = ingredientInputEl.val();
+    var dropDownVal = ingListSelectEl.val();
+    console.log(dropDownVal);
     if(ingredientVal !=="") {
         ingredientInputEl.val("");
         window.location.replace(`./search-results.html?i=${ingredientVal}`);
